63b35ea1ee7c671e55a4ae82040cd447efcb2af3
Guo ZHANG <iGuo19961110@gmail.com>
2016-06-18 23:21:53 +0800

    Add files via upload

--- decorator.py
+++ decorator.py
@@ -3,8 +3,37 @@
 from __future__ import print_function,unicode_literals    # import features of Python 3
 
 
+#----------module document----------
+
+__pyVersion__ = '2.7.9'
+
+__author__ = 'Guo Zhang'
+
+__contributors__ = ''
+
+__last_edit_date__ = '2016-6-18'
+
+__creation_date__ = '2016-6-18'
+
+__moduleVersion__ = '1.0'
+
+__doc__ = '''
+This is a decorator module for China's Prices Project
+'''
+
+
+#----------module document----------
+
+
+#----------module import----------
+
+# import system modules
 import time
 
+#----------module import----------
+
+
+#----------function definition----------
 
 def printTime(func):
     def _deco(*args,**kwargs):
@@ -15,9 +44,17 @@ def printTime(func):
         return func
     return _deco
 
+
 @printTime
 def func():
     time.sleep(1)
     
+#----------function definition----------
+    
+    
+#----------main function----------
+
 if __name__=="__main__":
     func()
+    
+#----------main function----------
